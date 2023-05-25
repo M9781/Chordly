9f058828b7c48c14067a774fe5596504f2c3a790
Rafał Borowski <moderator9781@gmail.com>
2023-05-25 16:28:40 +0200

    Add remaining-chords.js file ;)

---
+++ scripts/remaining-chords.js
@@ -0,0 +1,32 @@
+// ******************************************************
+//   Remaining Chords
+// ******************************************************
+
+async function changeRemainingChords() {
+  iteration++;
+  if (app.repeatChord <= iteration) iteration = 0;
+
+  remainingIterations = app.repeatChord - iteration;
+  remainingChordsElement.textContent = remainingIterations;
+}
+
+function startRemainingChords() {
+  remainingChordsElement.textContent = app.repeatChord;
+  const time = getRemainingChordsTime();
+  repeatChordInterval = setInterval(changeRemainingChords, time);
+  startSpin();
+  setSpinTime(time);
+}
+
+function stopRemainingChords() {
+  clearInterval(repeatChordInterval);
+  remainingChordsElement.textContent = app.repeatChord;
+  stopSpin();
+}
+
+function getRemainingChordsTime() {
+  const remainingChordsTime = (60 / app.BPM) * 1000 * getFractionsNumber();
+  return remainingChordsTime;
+}
+
+let iteration = 0;
